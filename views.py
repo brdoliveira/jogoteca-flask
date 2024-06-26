26cb3a3c89888702d2fcc6ad246ef0d1e3fd64b7
brdoliveira <brunoribeirooliveira64@gmail.com>
2024-06-26 01:17:44 -0300

    FEAT(views): Importando modulos

--- views.py
+++ views.py
@@ -1,6 +1,8 @@
 from flask import render_template, request, redirect, session, flash, url_for, send_from_directory
 from jogoteca import app, db
 from models import Jogos, Usuarios
+from helpers import recupera_imagem, deleta_arquivo
+import time
 
 @app.route('/')
 def index():
@@ -29,19 +31,20 @@ def criar():
     db.session.add(novo_jogo)
     db.session.commit()
 
-    arquivo = request.files ['arquivo']
+    arquivo = request.files['arquivo']
     upload_path = app.config['UPLOAD_PATH']
-    arquivo.save(f'{upload_path}/capa{novo_jogo.id}')
+    timestamp = time.time()
+    arquivo.save(f'{upload_path}/capa{novo_jogo.id}-{timestamp}.jpg')
 
     return redirect(url_for('index'))
 
 @app.route('/editar/<int:id>')
 def editar(id):
     if 'usuario_logado' not in session or session['usuario_logado'] == None:
-        return redirect(url_for('login', proxima=url_for('editar')))
+        return redirect(url_for('login', proxima=url_for('editar', id=id)))
     jogo = Jogos.query.filter_by(id=id).first()
-
-    return render_template('editar.html', titulo='Editando Jogo', jogo = jogo)
+    capa_jogo = recupera_imagem(id)
+    return render_template('editar.html', titulo='Editando Jogo', jogo=jogo, capa_jogo=capa_jogo)
 
 @app.route('/atualizar', methods=['POST',])
 def atualizar():
@@ -52,6 +55,13 @@ def atualizar():
 
     db.session.add(jogo)
     db.session.commit()
+
+    arquivo = request.files['arquivo']
+    upload_path = app.config['UPLOAD_PATH']
+    timestamp = time.time()
+    deleta_arquivo(id)
+    arquivo.save(f'{upload_path}/capa{jogo.id}-{timestamp}.jpg')
+
     return redirect(url_for('index'))
 
 @app.route('/deletar/<int:id>')
